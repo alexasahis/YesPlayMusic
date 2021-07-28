e911cf1efb6325079d928b7c1dbbe6a96cd68d1f
Alex <alexasahis@gmail.com>
2021-07-28 09:31:08 +0800

    add next-tarck-button to lyrics.vue

--- src/views/lyrics.vue
+++ src/views/lyrics.vue
@@ -191,6 +191,11 @@
           <svg-icon icon-class="arrow-down" />
         </button>
       </div>
+      <div class="next-track-button" @click="player.playNextTrack">
+        <button>
+          <svg-icon icon-class="next" />
+        </button>
+      </div>
     </div>
   </transition>
 </template>
@@ -664,6 +669,34 @@ export default {
 .close-button {
   position: fixed;
   top: 24px;
+  right: 24px;
+  z-index: 300;
+  border-radius: 0.75rem;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.28;
+  transition: 0.2s;
+  -webkit-app-region: no-drag;
+
+  .svg-icon {
+    color: var(--color-text);
+    padding-top: 5px;
+    height: 22px;
+    width: 22px;
+  }
+
+  &:hover {
+    background: var(--color-secondary-bg-for-transparent);
+    opacity: 0.88;
+  }
+}
+
+.next-track-button {
+  position: fixed;
+  top: 74px;
   right: 24px;
   z-index: 300;
   border-radius: 0.75rem;
